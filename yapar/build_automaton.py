f8f7988db68d54f204433677514bbc7826cbbfd1
xtsebas <thiagohugo2014@gmail.com>
2025-05-15 22:17:12 -0600

    wip: still working

---
+++ yapar/build_automaton.py
@@ -0,0 +1,30 @@
+from yapar.automaton.LR import Automaton
+from yapar.automaton.token_map import TOKEN_MAP
+
+def extract_symbols(grammar_dict, token_set):
+    all_rhs_symbols = set()
+    for prods in grammar_dict.values():
+        for prod in prods:
+            all_rhs_symbols.update(prod)
+
+    non_terminals = set(grammar_dict.keys())
+    terminals = (all_rhs_symbols - non_terminals)
+
+    # Filtrar para dejar solo los que están en los terminales
+    terminals = terminals.intersection(token_set)
+
+    return terminals, non_terminals
+
+def build_from_grammar(parsed_result):
+    grammar = parsed_result['grammar']
+    terminales = parsed_result['terminales']
+    start_symbol = next(iter(grammar))  # primer símbolo no terminal
+
+    # Agregar producción inicial aumentada
+    augmented_start = f"{start_symbol}'"
+    grammar[augmented_start] = [[start_symbol]]
+
+    terminals, non_terminals = extract_symbols(grammar, terminales)
+
+    automaton = Automaton(grammar, terminals, non_terminals, start_symbol)
+    return automaton
