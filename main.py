1a797e1d7c0513eb9b930a0746a27966126e9cea
josuemj <98498772+josuemj@users.noreply.github.com>
2025-05-21 22:54:48 -0600

    feat: integrate YALex lexer and token processing into the main flow

--- main.py
+++ main.py
@@ -14,21 +14,22 @@ from yapar.utils.follow import compute_follow
 from yapar.utils.build_slr_table import build_slr_table
 
 
-#parsed = parse_yapar_file('./examples/yapar/complex.yalp')
+parsed = parse_yapar_file('./examples/yapar/complex.yalp')
 
-#EJEMPLO EN CLASE
-grammar = {
-    'E': [['E', '+', 'T'], ['T']],
-    'T': [['T', '*', 'F'], ['F']],
-    'F': [['(', 'E', ')'], ['id']]
-}
+# NOTE: Move to test
+#EJEMPLO EN CLASE  
+# grammar = {
+#     'E': [['E', '+', 'T'], ['T']],
+#     'T': [['T', '*', 'F'], ['F']],
+#     'F': [['(', 'E', ')'], ['id']]
+# }
 
-terminales = {'+', '*', '(', ')', 'id'}
+# terminales = {'+', '*', '(', ')', 'id'}
 
-parsed = {
-    'terminales': terminales,
-    'grammar': grammar
-}
+# parsed = {
+#     'terminales': terminales,
+#     'grammar': grammar
+# }
 
 print("Terminales:", parsed['terminales'])
 print("Grammar:")
@@ -82,56 +83,57 @@ for state, transitions in GOTO.items():
 
 print("\n===============VERIFICACIÓN DE CADENA=======================\n")
 
+# NOTE: Move to test
 # Cadena: id + id * id → simula tokens (como saldrían del lexer)
-tokens = [
-    ('id', 'x'),
-    ('+', '+'),
-    ('id', 'y'),
-    ('*', '*'),
-    ('id', 'z'),
-]
+# tokens = [
+#     ('id', 'x'),
+#     ('+', '+'),
+#     ('id', 'y'),
+#     ('*', '*'),
+#     ('id', 'z'),
+# ]
 
 # Verificar con el parser SLR
-success = parse_tokens(tokens, ACTION, GOTO, start_symbol)
 
-if success:
-    print("Cadena aceptada.")
-else:
-    print("Cadena rechazada.")
         
-### TEST DEL YALEX
+### FLUJO YALEX - YAPAR
 
-#difficulty = "complex" # "easy", "complex"
-#
-#yalex_file = f"examples/yalex/{difficulty}.yalex" # temp change
-#input_file = f"examples/input_strings/{difficulty}.txt" # temp change
-#
-#with open(input_file, 'r', encoding='utf-8') as f:
-#   input_text = f.read()
-#
-#print("Texto a ver: ", input_text)
-#
-## Crear analizador léxico
-#lexer = YALexLexer(yalex_file)
-#lexer.build_dfa()
-##visualize_automaton(lexer)
-#
-#
-## Procesar entrada  
-#lexer.tokenize(input_text)  # Add debug parameter if supported
-#
-## Mostrar los tokens generados
-#print("\nTABLA DE TOKENS")
-#for token in lexer.tokens:
-#   
-#   # aca iria la interaccion con el Yapar :)
-#   
-#   print(token)
-#
-## Mostrar errores si los hay
-#if lexer.errors:
-#   print("\nErrores Léxicos Encontrados:")
-#   for error in lexer.errors:
-#       print(error)
-#
-#
+difficulty = "complex" # "easy", "complex"
+
+yalex_file = f"examples/yalex/{difficulty}.yalex" # temp change
+input_file = f"examples/input_strings/{difficulty}.txt" # temp change
+
+with open(input_file, 'r', encoding='utf-8') as f:
+  input_text = f.read()
+
+print("Texto a ver: ", input_text)
+
+# Crear analizador léxico
+lexer = YALexLexer(yalex_file)
+lexer.build_dfa()
+#visualize_automaton(lexer)
+
+
+# Procesar entrada  
+lexer.tokenize(input_text)  # Add debug parameter if supported
+
+# Mostrar los tokens generados
+print("\nTABLA DE TOKENS")
+for token in lexer.tokens:
+    print(token)
+    tokens = [(t['type'], t['value']) for t in lexer.tokens]
+
+    print("\n=============== PARSEANDO CON YAPar ===============\n")
+    success = parse_tokens(tokens, ACTION, GOTO, start_symbol)
+
+    if success:
+        print(" Cadena aceptada por el parser.")
+    else:
+        print(" Cadena rechazada por el parser.")
+
+# Mostrar errores (lexicos) si los hay
+if lexer.errors:
+  print("\nErrores Léxicos Encontrados:")
+  for error in lexer.errors:
+      print(error)
+
