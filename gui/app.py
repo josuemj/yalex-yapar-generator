9792938b178b9cacf84a22a0347e1b7814192104
josuemj <98498772+josuemj@users.noreply.github.com>
2025-05-22 23:00:07 -0600

    feat: add utility imports and update example options in UI

--- gui/app.py
+++ gui/app.py
@@ -1,4 +1,13 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import streamlit as st
+
+# Utils
+from utils.parser_utils import parse_yapar_file
+from yapar.utils.first import compute_first
+from yapar.utils.follow import compute_follow
+from yapar.utils.build_slr_table import build_slr_table
 
 st.set_page_config(page_title="YALex/YAPar Generator", layout="centered")
 
@@ -7,8 +16,9 @@ st.title("YALex / YAPar Generator UI")
 # Dropdown for selecting example type
 example_type = st.selectbox(
     "Choose YALex Specification Type:",
-    options=["Easy", "Hard", "Complex"]
+    options=["easy", "hard", "complex"]
 )
+
 
 # Text input area for writing or editing the specification
 spec_input = st.text_area(
